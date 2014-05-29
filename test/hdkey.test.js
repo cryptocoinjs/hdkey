6f0e671865a7e54374a94effb96673ae0a6191d3
JP Richardson <jprichardson@gmail.com>
2014-05-28 22:40:59 -0500

    tests ready

--- test/hdkey.test.js
+++ test/hdkey.test.js
@@ -1,12 +1,20 @@
-var hdkey = require('../')
+var HDKey = require('../')
 var fixtures = require('./fixtures')
 
 require('terst')
 
+function encode(buf) {
+  
+}
+
 describe('hdkey', function() {
   it('should properly derive the chain path', function() {
     fixtures.valid.forEach(function(f) {
-      
+      var hdkey = new HDKey(new Buffer(f.seed, 'hex'))
+      var childkey = hdkey.derive(f.path)
+
+      EQ (encode(childkey.private), f.private)
+      EQ (encode(childkey.public), f.public)
     })    
   })  
 })
