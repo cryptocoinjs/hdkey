352b9b69e1ad11e1b253a30f547c081f57903a15
JP Richardson <jprichardson@gmail.com>
2014-05-29 10:56:19 -0500

    implemented test encode method

--- test/hdkey.test.js
+++ test/hdkey.test.js
@@ -1,10 +1,14 @@
+var crypto = require('crypto')
+var bs58 = require('bs58')
 var HDKey = require('../')
 var fixtures = require('./fixtures')
 
 require('terst')
 
 function encode(buf) {
-  
+  var hash = crypto.createHash('sha256').update(buf).digest()
+  var chksum = crypto.createHash('sha256').update(hash).digest().slice(0,4)
+  return bs58.encode(Buffer.concat([buf, chksum]))
 }
 
 describe('hdkey', function() {
