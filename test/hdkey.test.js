5e592afeee54ccd9e46468359af5343cdfd70d9b
JP Richardson <jprichardson@gmail.com>
2014-06-16 19:46:27 -0500

    implemented method fromExtendedKey()

--- test/hdkey.test.js
+++ test/hdkey.test.js
@@ -23,6 +23,9 @@ describe('hdkey', function() {
         var hdkey = HDKey.fromMasterSeed(new Buffer(f.seed, 'hex'))
         var childkey = hdkey.derive(f.path)
 
+        if (encode(childkey.privateExtendedKey) == "xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j")
+          console.log(childkey.fingerprint.toString('16'))
+
         assert.equal(encode(childkey.privateExtendedKey), f.private)
         assert.equal(encode(childkey.publicExtendedKey), f.public)
       })    
@@ -60,6 +63,44 @@ describe('hdkey', function() {
       assert.equal(pub.length, 65)
       var hdkey = new HDKey()
       hdkey.publicKey = pub
+    })
+  })
+
+  describe('+ fromExtendedKey()', function() {
+    describe('> when private', function() {
+      it('should parse it', function() {
+        //m/0/2147483647'/1/2147483646'/2
+        var key = "xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j"
+        var keyBuffer = bs58.decode(key).slice(0, 78)
+        var hdkey = HDKey.fromExtendedKey(keyBuffer)
+        assert.equal(hdkey.versions.private, 0x0488ade4)
+        assert.equal(hdkey.versions.public, 0x0488b21e)
+        assert.equal(hdkey.depth, 5)
+        assert.equal(hdkey.parentFingerprint, 0x31a507b8)
+        assert.equal(hdkey.index, 2)
+        assert.equal(hdkey.chainCode.toString('hex'), '9452b549be8cea3ecb7a84bec10dcfd94afe4d129ebfd3b3cb58eedf394ed271')
+        assert.equal(hdkey.privateKey.toString('hex'), 'bb7d39bdb83ecf58f2fd82b6d918341cbef428661ef01ab97c28a4842125ac23')
+        assert.equal(hdkey.publicKey.toString('hex'), '024d902e1a2fc7a8755ab5b694c575fce742c48d9ff192e63df5193e4c7afe1f9c')
+        assert.equal(hdkey.identifier.toString('hex'), '26132fdbe7bf89cbc64cf8dafa3f9f88b8666220')
+      })
+    })
+
+    describe('> when public', function() {
+      it('should parse it', function() {
+        //m/0/2147483647'/1/2147483646'/2
+        var key = "xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt"
+        var keyBuffer = bs58.decode(key).slice(0, 78)
+        var hdkey = HDKey.fromExtendedKey(keyBuffer)
+        assert.equal(hdkey.versions.private, 0x0488ade4)
+        assert.equal(hdkey.versions.public, 0x0488b21e)
+        assert.equal(hdkey.depth, 5)
+        assert.equal(hdkey.parentFingerprint, 0x31a507b8)
+        assert.equal(hdkey.index, 2)
+        assert.equal(hdkey.chainCode.toString('hex'), '9452b549be8cea3ecb7a84bec10dcfd94afe4d129ebfd3b3cb58eedf394ed271')
+        assert.equal(hdkey.privateKey, null)
+        assert.equal(hdkey.publicKey.toString('hex'), '024d902e1a2fc7a8755ab5b694c575fce742c48d9ff192e63df5193e4c7afe1f9c')
+        assert.equal(hdkey.identifier.toString('hex'), '26132fdbe7bf89cbc64cf8dafa3f9f88b8666220')
+      })
     })
   })
 })
