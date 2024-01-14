f00088b0e0b0fc945e1c2cf5d7f22819a34c4c24
Alexander Pataridze <s.pataridze@gmail.com>
2024-01-14 17:20:17 +0400

    feat: public key api tests

--- test/hdkey.test.js
+++ test/hdkey.test.js
@@ -66,12 +66,36 @@ describe('hdkey', function () {
       hdkey.publicKey = pub
     })
 
+    it('should not throw if key is 33 bytes (compressed)', function () {
+      var priv = Buffer.from(fixtures.rawHex[0].private, 'hex')
+      var pub = curve.G.multiply(BigInteger.fromBuffer(priv)).getEncoded(true)
+      assert.equal(pub.length, 33)
+      var hdkey = new HDKey()
+      hdkey.publicKey = pub
+    })
+
     it('should not throw if key is 65 bytes (not compressed)', function () {
       var priv = secureRandom.randomBuffer(32)
       var pub = curve.G.multiply(BigInteger.fromBuffer(priv)).getEncoded(false)
       assert.equal(pub.length, 65)
       var hdkey = new HDKey()
       hdkey.publicKey = pub
+    })
+
+    it('should not throw if key is 65 bytes (not compressed)', function () {
+      var priv = Buffer.from(fixtures.rawHex[1].private, 'hex')
+      var pub = curve.G.multiply(BigInteger.fromBuffer(priv)).getEncoded(false)
+      assert.equal(pub.length, 65)
+      var hdkey = new HDKey()
+      hdkey.publicKey = pub
+    })
+
+    fixtures.rawHex.forEach(function (f) {
+      it('should convert to correct public key', function () {
+        var hdkey = new HDKey()
+        hdkey.privateKey = Buffer.from(f.private, 'hex')
+        assert.equal(hdkey.publicKey.toString('hex'), f.public)
+      })
     })
   })
 
